43d337ee91149f3f82e197be137a95d11f0eef74
BatoBalint <balint.bato@gmail.com>
2021-11-09 14:36:51 +0100

    kesz az alap feladat

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ var bgcolorInput;
 
 var resetBtn;
 
+var exampleDiv;
+var exampleP;
+
 var defText;
 var defFontsize;
 var defColor;
@@ -16,23 +19,42 @@ function init() {
   textError = document.getElementById('textError');
 
   textInput = document.getElementById('textInput');
+  textInput.addEventListener('input', exampleRefresh);
   defText = textInput.value;
 
   fontSizeInput = document.getElementById('fontSizeInput');
+  fontSizeInput.addEventListener('input', exampleRefresh);
   defFontsize = fontSizeInput.value;
 
   forecolorInput = document.getElementById('forecolorInput');
+  forecolorInput.addEventListener('input', exampleRefresh);
   defColor = forecolorInput.value;
 
   bgcolorInput = document.getElementById('bgcolorInput');
+  bgcolorInput.addEventListener('input', exampleRefresh);
   defBgcolor = bgcolorInput.value;
 
   resetBtn = document.getElementById('resetBtn');
   resetBtn.addEventListener('click', resetBtnClick);
+
+  exampleDiv = document.getElementById('exampleDiv');
+  exampleP = document.getElementById('exampleP');
+
+  exampleRefresh();
 }
 
 document.addEventListener('DOMContentLoaded', init);
 
 function resetBtnClick() {
-  alert("test");
+  textInput.value = defText;
+  fontSizeInput.value = defFontsize;
+  forecolorInput.value = defColor;
+  bgcolorInput.value = defBgcolor;
+}
+
+function exampleRefresh() {
+  exampleDiv.style.backgroundColor = bgcolorInput.value;
+  exampleP.innerHTML = textInput.value;
+  exampleP.style.color = forecolorInput.value;
+  exampleP.style.fontSize = fontSizeInput.value + "px";
 }
